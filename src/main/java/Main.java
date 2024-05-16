2fcb59b31de6da5e659baa2e0c4a8a2748b405d4
C_BO <827540030@qq.com>
2024-05-17 00:50:02 +0800

    test commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        String s = "hello world";
+
+        System.out.println(s + "hello world");
     }
 }
