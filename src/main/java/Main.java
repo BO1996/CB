0540ca378e051f5e9ce70123a82c97f7bf5b6221
C_BO <827540030@qq.com>
2024-05-21 22:48:10 +0800

    0521力扣每日一题

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,7 @@ public class Main {
         String s = "hello world";
 
         System.out.println(s + "hello world");
+        Solution solution = new Solution();
+        System.out.println("最大的可达数字为：" + solution.theMaximumAchievableX(3, 2));
     }
 }
